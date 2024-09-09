d47d038d2e283dfd572ca30faaa3c74ac085ee47
Zephrys_TheGreatest <froloff1830@gmail.com>
2024-09-09 19:49:17 +0300

    ADD | create new hierarchy of project, protect tokens and other private info

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod utils;
+use crate::utils::json_processing::json::*;
+
 use mongodb::{ 
     bson::{Document, doc},
     Client,
@@ -7,20 +10,11 @@ use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
-    const token: &str = "935459293:AAHI7XXWVeYehEDR_DCUo8uT4zDOoBX0jCs";
-
-    let uri = "mongodb://127.0.0.1:27017";
-    let client = Client::with_uri_str(uri).await?;
-    // Get a handle on the movies collection
-    let database = client.database("local");
-    let my_coll: Collection<Document> = database.collection("startup_log");
-    // Find a movie based on the title value
-    let value = my_coll.find(doc! {}).await?;
-    //println!("Found a value: {:#?}", value);
+    let secure_data: serde_json::Value = get_private_data();
+    let token: String = get_token(&secure_data);
 
     pretty_env_logger::init();
     log::info!("Starting throw dice bot...");
-
     let bot: Bot = Bot::new(token);
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
